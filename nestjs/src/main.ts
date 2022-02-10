8a113a87fb926df716db1b559698d60d04783340
Lucas Vinicius Sousa Silva <lucas2606.vinicius@gmail.com>
2022-02-10 20:52:04 -0300

    Atualizando porta do servidor nest e liberando cores para requisição

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 // Toda a aplicação, ao fim, se resume ao módulo principal, que ouve a porta indicada
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(3001);
 }
 bootstrap();
